2b5bc7ed80c027eb2c04b5d583caad50a375fc0f
ShaunZh <308826842@qq.com>
2018-07-13 12:20:32 +0800

    使用utils.http方法来获取电影信息

--- pages/movies/movies.js
+++ pages/movies/movies.js
@@ -23,42 +23,43 @@ Page({
     // top250
     const top250Url = `${app.globalData.g_doubanApiBaseUrl}/v2/movie/top250?start=0&count=3`;
     
-    this.getMoviesList(inTheatersUrl, 'inTheatersMovies', '正在热映');
-    this.getMoviesList(comingSoonUrl, 'comingSoonMovies', '即将上映');
-    this.getMoviesList(top250Url, 'top250Movies', 'top250');
-
+    utils.http(inTheatersUrl, (moviesList) => {
+      this.processDoubanData(moviesList, 'inTheatersMovies', '正在热映')
+    });
+    utils.http(comingSoonUrl, (moviesList) => {
+      this.processDoubanData(moviesList, 'comingSoonMovies', '即将上映')
+    });
+    utils.http(top250Url, (moviesList) => {
+      this.processDoubanData(moviesList, 'top250Movies', '豆瓣Top250')
+    });
   },
-  // 获取电影列表
-  getMoviesList(url, movieCategory, categoryName){
-    wx.request({
-      url,
-      method: 'GET',
-      data: {},
-      header: {
-        // 这里必须填写为 json， 不能填写 application/json
-        "Content-Type": "json"  
-      },
-      success: (res) => {
-        const temp = {};
-        const arr = res.data.subjects.map((item) => {
-          return {
-            id: item.id,  // 电影id
-            title: item.title,  //电影名 
-            rating: { 
-              average: item.rating.average, // 评分
-              stars: item.rating.stars, // 星
-              starsArray: utils.convertMovieStarsToArray(item.rating.stars)
-            },
-            image: item.images.large // 海报
-          };
-        });
-        temp[movieCategory] = {
-          movies: arr,
-          categoryName
 
-        };
-        this.setData(temp);
-      },
+  processDoubanData: function (moviesList, movieCategory, categoryName) {
+    const temp = {};
+    const arr = moviesList.subjects.map((item) => {
+      return {
+        id: item.id,  // 电影id
+        title: item.title.length > 6 ? item.title.substring(0, 6)+'...' : item.title,  //电影名 
+        rating: {
+          average: item.rating.average, // 评分
+          stars: item.rating.stars, // 星
+          starsArray: utils.convertMovieStarsToArray(item.rating.stars)
+        },
+        image: item.images.large // 海报
+      };
+    });
+    temp[movieCategory] = {
+      movies: arr,
+      categoryName
+
+    };
+    this.setData(temp);
+  },
+  onMoveMovieTap: function(event) {
+    const category = event.currentTarget.dataset.categorytitle;
+    console.log(event)
+    wx.navigateTo({
+      url: `more-movie/more-movie?category=${category}`,
     })
   }
 })
